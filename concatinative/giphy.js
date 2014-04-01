8547eb41be22d087c4a07b83a2a1df01ca9243ca
alwaysreed <splinterspool@yahoo.com>
2014-03-31 22:59:32 -0400

    I dont know what happened, but we're back to working

--- concatinative/giphy.js
+++ concatinative/giphy.js
@@ -4,11 +4,43 @@ var _ = require('underscore');
 var Q = require('q');
 
 module.exports = (function() {
+	var contract = {
+		translate: translate,
+		testDrive: testDrive
+	};
+
 	var api = {
 		key: 'dc6zaTOxFJmzC',
-		url: 'api.giphy.com',
-		defaultSearchEndpoint: '/v1/gifs/translate'
+		url: 'localhost',
+		port: 80,
+		endpoints: {
+			auth: 'session_process.html'
+		}
 	};
+
+	function testDrive() {
+
+		authenticate()
+			// TODO:
+			// .then(extractKey)
+			// .then(sendSomeData)
+			// .then(mergeUser)
+		/*
+			We want to walk the user through
+			a trivial use of the API.
+
+			1. Authenticate -> Aquire Key
+			2. Send some data
+			3. Merge user
+		*/
+	}
+
+	function authenticate() {
+		// TODO: Send an authentication request to
+		// AlwaysPrepped web app
+
+		// return promise of success
+	}
 
 	function queryString(data) {
 		return _.map(data, function(val, key) {
@@ -18,7 +50,7 @@ module.exports = (function() {
 
 	function apiRequest(settings) {
 		var settings = settings || { query: 'steve' },
-			fullUrl = api.url + (settings.endpoint || api.defaultSearchEndpoint),
+			fullUrl = api.url,
 			data = { s: settings.query, api_key: api.key },
 			deferred = Q.defer(),
 			options = {
@@ -37,28 +69,22 @@ module.exports = (function() {
 				response.on('end', function () {
 					deferred.resolve(JSON.parse(result));
 				});
-			},
-			// The actual request
-			req = http.request(options, callback);
+			};
 
-		req.end();
+		http.request(options, callback)
+			// End() causes the request to get sent,
+			// i.e. "End of message"
+			.end();
 
 		return deferred.promise;
 	}
 
-	return {
-		translate: function (query) {
-			return apiRequest({ query: query })
-	 			.then(function (data) {
-		 				if (_.isEmpty(data) || _.isEmpty(data.data) || _.isEmpty(data.data.images)) {
-		 					throw new Error("no results");
-		 					return;
-		 				}
+	function translate(query) {
+		return apiRequest({ query: query })
+ 			.then(function (data) {
+ 				/* Do something with resultant */
+ 			});
+ 	}
 
-		 				return {
-		 					src: data.data.images.fixed_width.url
-		 				};
-		 			});
-	 	}
-	};
+	return contract;
 })();
